6f5d79452bdf8fdbd8e77e167b7a3d00ea75cee0
reddevilcero <reddevil_cero@hotmail.com>
2020-06-22 14:17:07 +0100

    add ccreation documentation

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -3,3 +3,11 @@ class Cohort < ApplicationRecord
   belongs_to :campus
   belongs_to :course
 end
+
+
+# campus = Campus.find_or_create_by(name:'Online')
+
+# course = Course.create(title:'Software Engineering', description:'FullStack Dev')
+# course.campus = campus
+
+# Cohort.create(campus:campus, course:course , graduation:Date.new(2020,7), cohort_lead:'Alex')
