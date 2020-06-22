ab2d7bbdf28a007f4fb6c1c9ec0d7cb130b22e4b
reddevilcero <reddevil_cero@hotmail.com>
2020-06-22 11:45:04 +0100

    has many association create

--- app/models/campus.rb
+++ app/models/campus.rb
@@ -1,3 +1,4 @@
 class Campus < ApplicationRecord
+  has_many :campus_courses
   has_many :courses, through: :campus_courses
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,4 @@
 class Course < ApplicationRecord
+  has_many :campus_courses
   has_many :campus, through: :campus_courses
 end
