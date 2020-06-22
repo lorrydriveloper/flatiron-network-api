f98b8c55f6db9451dd6a4f37130e508a1ebc5e6d
reddevilcero <reddevil_cero@hotmail.com>
2020-06-22 13:55:20 +0100

    add bcrypt and secured password to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  has_secure_password
   belongs_to :cohort
   belongs_to :company
   has_one :address, as: :addressable
-  
 end
+
+User.create(
+  name: 'Pedro David',
+  surname: 'Garcia Lopez',
+  email: 'reddevil_cero@hotmail.com',
+  password: '0000',
+  is_admin: true,
+  graduate: false,
+  work_remotely: false
+)
