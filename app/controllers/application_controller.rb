e8286554b86f54ff1933c412b67033b07fdc34c8
reddevilcero <reddevil_cero@hotmail.com>
2020-06-23 18:17:24 +0100

    add autentication on controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,44 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
+  before_action :authorized
+
+  def encode_token(payload)
+    JWT.encode(payload, ENV['JWT'])
+  end
+
+  def decoded_token
+    if auth_header
+
+      token = auth_header.split(' ')[1]
+
+      begin
+        JWT.decode(token, ENV['JWT'], true, algorithm: 'HS256')
+        # JWT.decode => [{ "user_id"=>"1" }, { "alg"=>"HS256" }]
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
+
+  def auth_header
+    request.headers['Authorization']
+  end
+
+  def current_user
+    if decoded_token
+      user_id = decoded_token[0]['user_id']
+      @user = User.find_by(id: user_id)
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authorized
+    unless logged_in?
+      render json: { message: 'Please log in' }, status: :unauthorized
+    end
+ end
 end
