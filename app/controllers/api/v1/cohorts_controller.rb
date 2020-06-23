50b03f618a2de64f8e36215594e30790379b71f4
reddevilcero <reddevil_cero@hotmail.com>
2020-06-23 18:17:53 +0100

    return the serializer user on login or signup

--- app/controllers/api/v1/cohorts_controller.rb
+++ app/controllers/api/v1/cohorts_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::CohortsController < ApplicationController 
 
+  skip_before_action :authorized, only: [:index]
 
   def index
     @cohort = Cohort.all
--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,34 +1,35 @@
 # frozen_string_literal: true
 
 class Api::V1::SessionsController < ApplicationController
+  skip_before_action :authorized, only: %i[sign_up login]
   def sign_up
-    @user = User.new(user_params)
-    @user.address = Address.create(address_params)
-    if @user.save
-      render json: @user, status :created
+    user = User.new(user_params)
+    user.address = Address.create(address_params)
+    if user.save
+      token = encode_token(user_id: user.id)
+      render json: { user: UserSerializer.new(user), jwt: token }, status: :accepted
     else
       render json: {
         error: 'Unable to save entity',
-        message: "#{@user.errors.full_messages.join(', ')} ",
+        message: "#{user.errors.full_messages.join(', ')} ",
         status: :bad_request
       }
     end
   end
 
   def login
-    @user = User.find_by_email(login_params[:email])
+    user = User.find_by_email(login_params[:email])
 
-    if @user &&  @user.authenticate(login_params[:password])
-      render json: @user
+    if user&.authenticate(login_params[:password])
+      token = encode_token(user_id: user.id)
+      render json: { user: UserSerializer.new(user), jwt: token }, status: :accepted
     else
       render json: {
         error: 'Unable to authenticate',
-        message: "User or Password is wrong",
+        message: 'User or Password is wrong',
         status: :not_acceptable
       }
     end
-    
-
   end
 
   private
