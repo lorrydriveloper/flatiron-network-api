58beacc0d7ba3e444df3373e14015d4b04d3e9c2
reddevilcero <reddevil_cero@hotmail.com>
2020-06-23 12:57:20 +0100

    handle creation of user sign up

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,9 +1,29 @@
-class Api::V1::SessionsController < ApplicationController 
+# frozen_string_literal: true
 
-
-
+class Api::V1::SessionsController < ApplicationController
   def sign_up
-    pry
+    @user = User.new(user_params)
+    @user.address = Address.create(address_params)
+    if @user.save
+      render json: @user
+    else
+      render json: {
+        error: 'Unable to save entity',
+        message: "#{@user.errors.full_messages.join(', ')} ",
+        status: :bad_request
+      }
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:session).permit(:name, :surname, :email, :password,:cohort_id)
+  end
+
+  def address_params
+    params.require(:session).permit(:plus_code, :street, :postcode, :city,
+                                    :country, :state)
   end
 
 
